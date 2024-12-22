7a16046f544d91342f75a5dd6a778deae32e596a
Jatsekku <jatsekku2@gmail.com>
2024-12-22 17:47:28 +0100

    feat(advanced-js): Add setTimeout 02 challenge

---
+++ advanced-js/set-timeout-02ch.js
@@ -0,0 +1,11 @@
+function logAnswer() {
+    console.log(`The answer is ?????? of course! If you got that right, giver yourself ?????? points.`)
+}
+
+console.log('What is the capital of Peru?')
+/*
+Challenge:
+    1. After a 3 second delay, call the 'logAnswer' function.
+    2. Make sure 'logAnswer' has all the info it needs. 
+       The answer is 'Lima' and it's 10 points for getting it right. 
+*/
