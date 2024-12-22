2c0ce3dea0295645e25f588e66a697f36ab3d4fb
Jatsekku <jatsekku2@gmail.com>
2024-12-22 17:39:33 +0100

    feat(advanced-js): Add object destructing 01 solution

---
+++ advanced-js/object-destructing-01sol.js
@@ -0,0 +1,11 @@
+const dreamHoliday = {
+   destination: 'Austin, Texas',
+   activity: 'visit the Tesla HQ',
+   accommodation: 'a luxury ranch',
+   companion: 'Elon Musk'
+}
+
+const {destination, activity, accommodation, companion} = dreamHoliday
+
+console.log(`I would love to go to ${destination} to ${activity}.
+            I'd sleep in ${accommodation} and hang out with ${companion} all day.`)
