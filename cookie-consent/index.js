0a7354ed7e8f316ff197930c22c94fdcbb8362ad
Jatsekku <jatsekku2@gmail.com>
2024-12-22 12:55:20 +0100

    feat(cookie-consent): Add Accept/Decline buttons swapping

--- cookie-consent/index.js
+++ cookie-consent/index.js
@@ -2,6 +2,8 @@ const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
+const modalDeclineBtn = document.getElementById('modal-decline-btn')
+const modalChoiceBtns = document.getElementById('modal-choice-btns')
 
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -9,6 +11,10 @@ setTimeout(function(){
 
 modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
+})
+
+modalDeclineBtn.addEventListener('mouseenter', function(){
+    modalChoiceBtns.classList.toggle('modal-choice-btns-reverse')
 })
 
 consentForm.addEventListener('submit', (e) => {
