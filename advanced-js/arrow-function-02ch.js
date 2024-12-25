f1a3876fecbdbc4356471f95529790a99191ff39
Jatsekku <Jatsekku2@gmail.com>
2024-12-25 15:38:39 +0100

    feat(advanced-js): Add arrow function 02 challenge

---
+++ advanced-js/arrow-function-02ch.js
@@ -0,0 +1,12 @@
+const speedWarning = speed => `You are going at ${speed} mph!`
+
+console.log(speedWarning(30, 40))
+
+/*
+Challenge
+1. Refactor this function so it only warns drivers 
+   who are going over the speed limit.
+2. The function now needs to take in two parameters. 
+   The first is the speed limit, the second is the 
+   driver's actual speed.
+*/
