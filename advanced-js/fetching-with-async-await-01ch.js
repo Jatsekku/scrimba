04315a6078e75aa9f2547fcdf80879751de0f9cc
Jatsekku <jatsekku2@gmail.com>
2024-12-29 18:18:50 +0100

    feat(advanced-js): Add fetching with async/await 01 challenge

---
+++ advanced-js/fetching-with-async-await-01ch.js
@@ -0,0 +1,10 @@
+/*
+Challenge:
+    1. Make a fetch request to the "Bored" API: 
+         Base URL: https://apis.scrimba.com/bored/api
+         Endpoint: /activity
+    2. Log an object containing an activity suggestion 
+       to the console.
+    ⚠️ Make sure you use the async/await method!
+    🛟 hint.md for help!
+*/
