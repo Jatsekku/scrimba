4b0331d8472a877e2bb9819563d3cb4d0761583a
Jatsekku <jatsekku2@gmail.com>
2024-12-29 21:44:05 +0100

    feat(advanced-js): Add next level API 01 challenge

---
+++ advanced-js/next-level-api-01ch.js
@@ -0,0 +1,7 @@
+/*
+Base URL: https://apis.scrimba.com/jsonplaceholder
+Endpoint: /posts
+Challenge:
+1. Make a fetch request to get all of the available posts.
+⚠️ Remember to handle all errors!
+*/
