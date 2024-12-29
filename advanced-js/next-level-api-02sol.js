d614fb97144ab97888bdd7354d92579c3df632d4
Jatsekku <jatsekku2@gmail.com>
2024-12-29 21:59:59 +0100

    feat(advanced-js): Add next level API 02 solution

---
+++ advanced-js/next-level-api-02sol.js
@@ -0,0 +1,34 @@
+async function foo() {
+    try {
+        const response = await fetch('https://apis.scrimba.com/jsonplaceholder/posts',
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    tile: 'Holiday Nightmares',
+                    body: 'When I was kidnapped in Scotland…',
+                    userId: 1,
+                }),
+            })
+        if (!response.ok) {
+            throw new Error('There was a problem with the API')
+        }
+        const data = await response.json()
+        console.log(data)
+    } catch(err) {
+        console.log(err)
+    }  
+}
+
+foo()
+
+/*
+Challenge:
+1. Using the code snippet in the slide, add a body 
+   property to the object we are passing with the 
+   fetch request. I want you to create a new post 
+   with the title “Holiday Nightmares” and the body 
+   “When I was kidnapped in Scotland…” 
+   
+   In the console, you should see and object with an 
+   ID, for example: {id: 101}
+*/
