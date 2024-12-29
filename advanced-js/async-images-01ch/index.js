3441b02feca1d70b4bb691fcaa9f1dc294c0e1cd
Jatsekku <jatsekku2@gmail.com>
2024-12-29 22:45:01 +0100

    feat(advanced-js): Add asynchronous images 01 challenge

---
+++ advanced-js/async-images-01ch/index.js
@@ -0,0 +1,12 @@
+const image = new Image()
+image.src = "http://..."
+
+console.log(image)
+
+/*
+Challenge:
+1. Create two event listeners. One should listen 
+   out for the image loading and log “Image has 
+   loaded”. The other should listen for an error 
+   and log “Image has NOT loaded”.
+*/
