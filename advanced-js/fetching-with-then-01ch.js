70485e25b0f652732cb09240cc213f834e382319
Jatsekku <jatsekku2@gmail.com>
2024-12-29 17:38:39 +0100

    feat(advanced-js): Add fetching with then() 01 challenge

---
+++ advanced-js/fetching-with-then-01ch.js
@@ -0,0 +1,9 @@
+/*
+Challenge:
+    1. Make a fetch request to the "Bored" API: 
+         Base URL: https://apis.scrimba.com/bored/api
+         Endpoint: /activity
+    2. Log an object containing an activity suggestion 
+       to the console.
+    🛟 hint.md for help!
+*/
