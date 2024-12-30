3fa54ce0dea975aba9d3119d855bfdaeb98ae387
Jatsekku <jatsekku2@gmail.com>
2024-12-30 23:02:02 +0100

    feat(advanced-js): Add asynchronous images 01 solution

---
+++ advanced-js/async-images-01sol/index.js
@@ -0,0 +1,7 @@
+const image = new Image()
+image.src = "https://scrimba.ams3.cdn.digitaloceanspaces.com/assets/courses/gadvancedjs/scenic1.jpg"
+
+console.log(image)
+
+image.addEventListener('load', () => console.log('Image has loaded'))
+image.addEventListener('error', () => console.log('Image has NOT loaded'))
