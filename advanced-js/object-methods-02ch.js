cb22b05449a6ef911078dfe08e981cd899ec80ca
Jatsekku <jatsekku2@gmail.com>
2025-01-02 12:38:56 +0100

    feat(advanced-js): Add object methods 02 challenge

---
+++ advanced-js/object-methods-02ch.js
@@ -0,0 +1,14 @@
+const books = {
+    "b001": { title: "To Kill a Mockingbird", price: 18.99, isAvailable: true },
+    "b002": { title: "1984", price: 15.99, isAvailable: false },
+    "b003": { title: "The Great Gatsby", price: 12.49, isAvailable: true },
+    "b004": { title: "Moby Dick", price: 22.50, isAvailable: false }
+}
+
+/*
+Challenge:
+  1. Create an array of values using Object.values
+  2. Iterate over it and log out each book price.
+*/
+  
+const bookValues = 
