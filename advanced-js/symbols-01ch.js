13a75051cd18537585043d4767a179eadfa4fd04
Jatsekku <jatsekku2@gmail.com>
2025-01-03 18:16:47 +0100

    feat(advanced-js): Add symbols 01 challenge

---
+++ advanced-js/symbols-01ch.js
@@ -0,0 +1,13 @@
+/*
+Challenge:
+    1. Add a hidden property to the book object 
+       holding a librarian’s note to say the book 
+       has gone missing. 
+    2. Log out the new property.
+*/
+
+const book = {
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    year: 1951
+}
