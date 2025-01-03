8718d91fef80b63267754dba3610d2bfa0a8cf3c
Jatsekku <jatsekku2@gmail.com>
2025-01-03 16:03:53 +0100

    feat(advanced-js): Add getters and setters 01 challenge

---
+++ advanced-js/getters-setters-01ch.js
@@ -0,0 +1,31 @@
+class Holiday {    
+/*
+Challenge:
+    1. Make 'price' a private field.
+    2. Create a getter for price which appends a $ sign 
+        to the front and displays it to a max of 2 decimal 
+        places. 
+    3. Create a setter for price which updates price with a 
+        new price.
+    4. Test! 
+*/
+    #destination
+    constructor(destination, price) {
+        this.#destination = destination
+        this.price = price
+    }
+
+    get destination() {
+        return this.#destination
+    }
+
+    set destination(newDestination) {
+        if (typeof newDestination !== 'string' || newDestination.length <= 0){
+            throw new Error('Destination not valid')
+        }
+        this.#destination = newDestination
+    }
+}
+
+const safari = new Holiday('Kenya', 1000)
+console.log(safari.price)
