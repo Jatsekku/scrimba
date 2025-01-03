fd55c48aef749e49c3b603e7f8e64ca1ceb7b01b
Jatsekku <jatsekku2@gmail.com>
2025-01-03 18:56:59 +0100

    feat(advanced-js): Add map object 01 note

---
+++ advanced-js/map-object-01note.js
@@ -0,0 +1,16 @@
+const athlete1 = { name: "Alice", age: "50" }
+const athlete2 = { name: "Dave", age: "51" }
+const athlete3 = { name: "Nicky", age: "49" }
+
+const finishers = new Map()
+
+finishers.set(athlete1, 10000)
+finishers.set(athlete2, 10200)
+finishers.set(athlete3, 9800)
+
+finishers.forEach((value, key)=> console.log(key.name, value))
+console.log(finishers.size)
+console.log(finishers.get(athlete1))
+console.log(finishers.get(athlete2))
+finishers.delete(athlete3)
+finishers.forEach((value, key)=> console.log(key.name, value))
