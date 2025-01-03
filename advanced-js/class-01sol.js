bb716e470668dcc9f2c4175e949efd649d693c9d
Jatsekku <jatsekku2@gmail.com>
2025-01-03 13:07:47 +0100

    feat(advanced-js): Add class 01 solution

---
+++ advanced-js/class-01sol.js
@@ -0,0 +1,14 @@
+class Character {
+    constructor(name) {
+        this.name = name
+        this.collectedItemsArr = []
+    }
+    
+    addItem(item) {
+        this.collectedItemsArr.push(item)
+        console.log(`${this.name} now has: ${this.collectedItemsArr.join(', ')}`)
+    }
+}
+
+const wizard = new Character('Merlin')
+wizard.addItem('a wand')
