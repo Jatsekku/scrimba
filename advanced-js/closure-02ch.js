afc8f15515f349ba22c0d22419ffe97be1392629
Jatsekku <jatsekku2@gmail.com>
2025-01-03 21:18:00 +0100

    feat(advanced-js): Add closure 02 challenge

---
+++ advanced-js/closure-02ch.js
@@ -0,0 +1,27 @@
+/* 
+Challenge: 
+    1. Write a function that simulates a simple bank account. 
+       The function should store the balance and enable users  
+       to deposit money, withdraw money, and check the balance.
+
+Requirements:
+
+- The account balance should start at zero.
+
+- The function should return an object containing 
+  3 functions. One for depositing money, one for 
+  withdrawing money, and a getBalance function which 
+  logs the balance and the account holder's name.
+
+- Deposits should add to the balance, and withdrawals should subtract 
+  from it. 
+*/
+
+function createBankAccount() {
+    // Write logic here
+}
+    
+const daveAccount = createBankAccount()
+const wendyAccount = createBankAccount()
+    
+// Test your code by calling the functions.
