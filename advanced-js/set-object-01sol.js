48808cad02612dec4b0cb0fa77173a71e3caa425
Jatsekku <jatsekku2@gmail.com>
2025-01-03 20:29:50 +0100

    feat(advanced-js): Add set object 01 solution

---
+++ advanced-js/set-object-01sol.js
@@ -0,0 +1,13 @@
+const postTags = new Set()
+
+function addTag(newTag) {
+    postTags.add(newTag)
+}
+
+addTag('history')
+addTag('romans')
+addTag('sociology')
+addTag('history')
+addTag('history')
+
+postTags.forEach((tag) => console.log(tag))
