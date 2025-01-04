cba18ee52f8100a9c5ffb2e4969e0168436203d9
Jatsekku <jatsekku2@gmail.com>
2025-01-04 22:16:10 +0100

    feat(react-basics/ReactFacts): Style Navbar component

--- react-basics/react-facts/components/Navbar.jsx
+++ react-basics/react-facts/components/Navbar.jsx
@@ -1,5 +1,10 @@
 export default function Navbar() {
     return (
-        <h1>Navbar component goes here</h1>
+        <header>
+            <nav>
+                <img src='../react-logo.png' alt='React logo'/>
+                <span>ReactFacts</span>
+            </nav>
+        </header>
     )
 }
