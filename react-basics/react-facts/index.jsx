de88ec50e0668c7fc4874b4f8602df9215c83b67
Jatsekku <jatsekku2@gmail.com>
2025-01-04 21:50:19 +0100

    feat(react-basics/ReactFacts): Initial components

--- react-basics/react-facts/index.jsx
+++ react-basics/react-facts/index.jsx
@@ -1,16 +1,7 @@
-import {createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
+import App from './App'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
-  <main>
-    <img src='react-logo.png' width='40px'></img>
-    <h1>Fun facts abut React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally createdy by Jordan Walke</li>
-      <li>Has well over 100K stars on GitHub</li>
-      <li>Is maintained by Meta</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </main>
+  <App />
 )
